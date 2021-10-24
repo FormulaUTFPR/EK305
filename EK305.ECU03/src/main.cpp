cd877264ac807070ba2ef75a69279ebfdf7595d0
Joas <js316410@gmail.com>
2021-10-23 21:52:08 -0300

    Auto stash before merge of "main" and "origin/main"

--- EK305.ECU03/src/main.cpp
+++ EK305.ECU03/src/main.cpp
@@ -315,7 +315,12 @@ void taskBrakePedalPos()
   {
     int position = analogRead(BrakePedalPosPIN); //Le o valor de posicao do pedal de freio
 
-    position = map(position, 0, 1023, 0, 100);//Converte os bits na posicao do pedal de freio(0% a 100%)
+    position = map(position, 0, 1023, 100, 0);//Converte os bits na posicao do pedal de freio(0% a 100%)
+<<<<<<< Updated upstream
+=======
+
+    //Serial.println(position);
+>>>>>>> Stashed changes
 
     BrakePedalPos.data[0] = position&0xFF;
 
